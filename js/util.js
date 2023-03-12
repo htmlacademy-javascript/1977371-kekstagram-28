1e5be7a854be682a491d41b70d852636fb2f69bf
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2023-03-12 20:06:52 +0600

    fixed bugs in functions

---
+++ js/util.js
@@ -0,0 +1,32 @@
+// функция для генерации случайного целого числа из выбранного интервала
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createRandomUniqId = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+// likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
+function getRandomIntInc(min, max) {
+  const a = Math.ceil(min);
+  const b = Math.floor(max);
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+export {getRandomPositiveInteger, createRandomUniqId, getRandomIntInc};
